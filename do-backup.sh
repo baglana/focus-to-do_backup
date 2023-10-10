8ec7bdc872ea8ba100661881c5e0254bf6063609
baglana <102351104+baglana@users.noreply.github.com>
2023-10-10 16:19:06 +0600

    do-backup.sh: add condition to check if dir exists

--- do-backup.sh
+++ do-backup.sh
@@ -1,41 +1,50 @@
 # A bash script to backup Focus To-Do Chrome extension files
 #!/bin/bash
 
-# Create backup directory
+# Check if dir with today's date exists
 base_dir=$(pwd)
 today=$(date +'%Y-%m-%d')
 backup_dir="${base_dir}/${today}"
-mkdir $backup_dir
 
-chrome_dir="/home/bag/.config/google-chrome"
+if [ -d $backup_dir ]
+then
+  echo "Directory ${today}/ exists. No new backup done."
+else
+  echo "Starting backup..."
 
-# Cannot use list from find because of space characters in extension folder names
-#paths_list=$(find $chrome_dir -name "*ngceodoilcgpmkijopinlkmohnfifjfb*")
+  # Create backup directory
+  mkdir $backup_dir
 
-# Declare a string array with type
-declare -a relPathsArray=(
-"Default/IndexedDB/chrome-extension_ngceodoilcgpmkijopinlkmohnfifjfb_0.indexeddb.leveldb"
-"Default/Local Extension Settings/ngceodoilcgpmkijopinlkmohnfifjfb"
-"Default/Sync Extension Settings/ngceodoilcgpmkijopinlkmohnfifjfb"
-#"Default/Extensions/ngceodoilcgpmkijopinlkmohnfifjfb"
-"extensions_crx_cache/ngceodoilcgpmkijopinlkmohnfifjfb_1.856545d6bcb2063e33bddbd9365c86c6e1df8a673a6cf6aa46e42c2d9bf9698d"
-)
+  chrome_dir="/home/bag/.config/google-chrome"
 
-# Loop through needed extension directories
-for rel_path in "${relPathsArray[@]}"
-  do
-    part1=$(dirname "$rel_path")
-    part2=$(basename "$rel_path")
-#    echo "part1: $part1"
-#    echo "part2: $part2"
+  # Cannot use list from find because of space characters in extension folder names
+  #paths_list=$(find $chrome_dir -name "*ngceodoilcgpmkijopinlkmohnfifjfb*")
 
-    # Create destinaiton directory tree
-    dest="${backup_dir}/${part1}"
-    echo -e "dest: $dest"
-    mkdir -p "$dest"
+  # Declare a string array with type
+  declare -a relPathsArray=(
+  "Default/IndexedDB/chrome-extension_ngceodoilcgpmkijopinlkmohnfifjfb_0.indexeddb.leveldb"
+  "Default/Local Extension Settings/ngceodoilcgpmkijopinlkmohnfifjfb"
+  "Default/Sync Extension Settings/ngceodoilcgpmkijopinlkmohnfifjfb"
+  #"Default/Extensions/ngceodoilcgpmkijopinlkmohnfifjfb"
+  "extensions_crx_cache/ngceodoilcgpmkijopinlkmohnfifjfb_1.856545d6bcb2063e33bddbd9365c86c6e1df8a673a6cf6aa46e42c2d9bf9698d"
+  )
 
-    # Copy files from chrome directory to created backup directory
-    src="${chrome_dir}/${rel_path}"
-    echo -e "src: $src\n\n"
-    cp -a "${src}" "${dest}/"
-  done
+  # Loop through needed extension directories
+  for rel_path in "${relPathsArray[@]}"
+    do
+      part1=$(dirname "$rel_path")
+      part2=$(basename "$rel_path")
+  #    echo "part1: $part1"
+  #    echo "part2: $part2"
+
+      # Create destinaiton directory tree
+      dest="${backup_dir}/${part1}"
+      echo -e "dest: $dest"
+      mkdir -p "$dest"
+
+      # Copy files from chrome directory to created backup directory
+      src="${chrome_dir}/${rel_path}"
+      echo -e "src: $src\n\n"
+      cp -a "${src}" "${dest}/"
+    done
+fi
